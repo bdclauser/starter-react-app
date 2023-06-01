f93384d56ed73a5ccedd9a7646589234d945f3c7
bdclauser <pzljug@hotmail.com>
2023-06-01 01:12:18 -0400

    updated secrets

--- aws_controller.py
+++ aws_controller.py
@@ -1,6 +1,26 @@
 import boto3
+from decouple import config
 
-dynamo_client = boto3.client("dynamodb")
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+REGION_NAME = config("REGION_NAME")
+OUTPUT_FORMAT = config("OUTPUT_FORMAT")
+
+dynamo_client = boto3.client(
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
+    output_format=OUTPUT_FORMAT,
+)
+
+resources = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
+    output_format=OUTPUT_FORMAT,
+)
 
 
 def get_items():
